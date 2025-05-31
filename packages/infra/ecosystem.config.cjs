8fc9655641f337a96f16082c04eaf9c228c8e6f2
t11s <greenfeatherhelp@gmail.com>
2025-05-30 22:00:39 -0700

    feat: add time to logs

--- packages/infra/ecosystem.config.cjs
+++ packages/infra/ecosystem.config.cjs
@@ -5,18 +5,21 @@ module.exports = {
       script: "pnpm",
       args: "start:poke",
       cron_restart: "0 * * * *", // Restart every hour.
+      time: true,
     },
     {
       name: "indexer",
       script: "pnpm",
       args: "start:indexer",
       cron_restart: "0 * * * *", // Restart every hour.
+      time: true,
     },
     {
       name: "auth",
       script: "pnpm",
       args: "start:auth",
       cron_restart: "0 * * * *", // Restart every hour.
+      time: true,
     },
   ],
 };
