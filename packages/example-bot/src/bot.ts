d9e4bed68b1b3a2d679dd0d6396c77353b082dc9
t11s <greenfeatherhelp@gmail.com>
2025-06-01 15:31:05 -0700

    feat: working state parsing bot

--- packages/example-bot/src/bot.ts
+++ packages/example-bot/src/bot.ts
@@ -1,14 +1,17 @@
 import "dotenv/config";
 import cliProgress from "cli-progress";
 import { http, fallback, webSocket, createPublicClient } from "viem";
-import { Entity } from "../../client/src/utils/game/entityLib";
-import { forwardStateTo, LiveState, parseSyncStateGivenTables } from "../../client/src/utils/sync";
-import { GameConfig } from "../../client/src/utils/game/configLib";
 
+import { forwardStateTo, LiveState, parseSyncStateGivenTables } from "../../client/src/utils/sync";
+import { EntityType } from "../../client/src/utils/game/entityLib";
+import { GameConfig } from "../../client/src/utils/game/configLib";
 import { stash } from "../../client/src/mud/stash";
-import { getRecord, getTable, Stash } from "@latticexyz/stash/internal";
-import { SyncProgress, syncToStash } from "@latticexyz/store-sync/internal";
+import { timeWad } from "../../client/src/utils/timeLib";
 import { ODYSSEY_CHAIN } from "../../client/src/utils/chains";
+
+import { syncToStash } from "@latticexyz/store-sync/internal";
+import { Stash } from "@latticexyz/stash/internal";
+
 import Worlds from "../../contracts/worlds.json";
 
 const chain = ODYSSEY_CHAIN;
@@ -16,35 +19,42 @@ const WORLD_ADDRESS = Worlds[chain.id]?.address as `0x${string}`;
 if (!WORLD_ADDRESS) throw new Error(`No world address found for chain ${chain.id}`);
 const START_BLOCK = Worlds[chain.id]!.blockNumber!;
 
-const publicClient = createPublicClient({
-  chain,
-  transport: fallback([webSocket(), http()]),
-  pollingInterval: 100,
-  cacheTime: 100,
-});
+function onBlock(liveState: LiveState, gameConfig: GameConfig, wadTime: bigint) {
+  const { lines, gameState } = liveState;
 
-const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  // <Do something with the processed game state here>
 
-function botDecision(
-  liveState: LiveState,
-  gameConfig: GameConfig,
-  wadTime: bigint
-): [string, string] {
-  // TODO
-  return ["up", "right"]; // [vertical, horizontal]
+  // Demo: Visualize each line by printing players with their usernames.
+  lines.forEach((line, idx) => {
+    const players = line
+      .filter(
+        (entity) => entity.etype === EntityType.ALIVE && gameState.usernames.has(entity.entityId)
+      )
+      .map((entity) => {
+        return gameState.usernames.get(entity.entityId);
+      });
+
+    console.log(
+      `Line ${idx} has players: ${players.length > 0 ? players.join(", ") : "No players"}`
+    );
+  });
 }
 
 export async function main() {
+  const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
   const { storedBlockLogs$ } = await syncToStash({
     stash: stash as Stash,
     startSync: true,
     address: WORLD_ADDRESS,
     startBlock: BigInt(START_BLOCK ?? 0),
-    publicClient: publicClient as any,
+    publicClient: createPublicClient({
+      chain,
+      transport: fallback([webSocket(), http()]),
+      pollingInterval: 100,
+      cacheTime: 100,
+    }),
     indexerUrl: chain.indexerUrl,
   });
-
-  // Initialize progress bar
   progressBar.start(100, 0);
 
   storedBlockLogs$.subscribe(() => {
@@ -54,27 +64,25 @@ export async function main() {
       if (progressBar.isActive) {
         progressBar.update(100);
         progressBar.stop();
-        console.log("Caught up!");
+        console.log("\n✅Caught up!");
       }
 
-      const { gameConfig, lines, lineStates, gameState } = data;
-
       const syncedState = {
-        lastSyncedTime,
+        lastSyncedTime: performance.now(),
         lastProcessedTime: -1n,
         lines: data.lines,
         lineStates: data.lineStates,
         gameState: data.gameState,
       };
 
-      const liveState = forwardStateTo(syncedState, gameConfig, false, null, {
-        stopAtIteration: 99999999999999,
+      const liveState = forwardStateTo(syncedState, data.gameConfig, false, null, {
+        stopAtIteration: null,
         stopAtTimestampWad: null,
       });
 
-      console.log("Got block!", Number(syncProgress.latestBlockNumber));
+      console.log("\n📥 Got block:", Number(syncProgress.latestBlockNumber), "\n");
 
-      const [vertical, horizontal] = botDecision(liveState, gameConfig, wadTime);
+      onBlock(liveState, data.gameConfig, timeWad());
     } else {
       if (syncProgress.step === "snapshot") progressBar.update(0);
       else progressBar.update(Math.round(syncProgress.percentage));
@@ -82,6 +90,6 @@ export async function main() {
   });
 }
 
-console.log("Starting bot... (this may take a couple seconds)", WORLD_ADDRESS, START_BLOCK);
+console.log("🤖 Starting bot... (this may take a couple seconds)", WORLD_ADDRESS, START_BLOCK);
 console.log();
 await main();
